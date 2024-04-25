f2dc2439f8b6af091da86032d67f3019f868e933
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-04-25 10:27:52 +0530

    Array Creation

---
+++ Array/ArrayCreation.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayCreation {
+
+	public static void main(String[] args) {
+		// Create a Scanner object to take input from the user
+		Scanner sc = new Scanner(System.in);
+
+		// Ask the user to enter the size of the array
+		System.out.println("Enter The Size of Array ");
+		int size = sc.nextInt(); // Read the size of the array from the user
+
+		// Create an array with the specified size
+		int arr[] = new int[size];
+
+		// Ask the user to enter the elements of the array
+		System.out.println("Enter The Elements ");
+		// Iterate over the array to read each element from the user
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt(); // Read the element from the user and store it in the array
+
+		}
+
+		// Close the Scanner object to prevent resource leak
+		sc.close();
+	}
+}
