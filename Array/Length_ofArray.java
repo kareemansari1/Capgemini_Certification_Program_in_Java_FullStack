b8a967ae006502a8e7038112586a5dbe1ecf484a
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-05-02 10:28:55 +0530

    Length of Array

---
+++ Array/Length_ofArray.java
@@ -0,0 +1,12 @@
+package Array_Codes;
+
+public class Length_ofArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println(" Number of element present in array is " + arr.length);
+
+	}
+
+}
