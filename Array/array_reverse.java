4cef4668b48cfb243027ccf8038efa3eb6ab85de
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-05-04 20:00:44 +0530

    Reverse Array

---
+++ Array/array_reverse.java
@@ -0,0 +1,14 @@
+package Array_Codes;
+
+public class array_reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = new int[] { 1, 2, 3, 4 };
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+}
