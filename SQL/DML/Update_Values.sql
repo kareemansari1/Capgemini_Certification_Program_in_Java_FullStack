8edaf1054afd048917c876f7298c79c79336d132
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-05-29 21:33:18 +0530

    Update Values

---
+++ SQL/DML/Update_Values.sql
@@ -0,0 +1,4 @@
+SELECT * FROM demo.student;
+insert into student values(4,'Zeba',28,'zebaansari@gmail.com',9876543210,'Maharashtra','Yeola','India');
+select * from student;
+update student set studentname='Mazhar' where studentid=2;
