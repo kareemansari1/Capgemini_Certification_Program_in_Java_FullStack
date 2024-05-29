f28d752b8ad56dded3c46c4fb810bbb275eaecf5
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-05-29 21:36:34 +0530

    Delete Values

---
+++ SQL/DML/Delete_Values.sql
@@ -0,0 +1,5 @@
+SELECT * FROM demo.student;
+insert into student values(4,'Zeba',28,'zebaansari@gmail.com',9876543210,'Maharashtra','Yeola','India');
+select * from student;
+update student set studentname='Mazhar' where studentid=2
+delete from student where studentid=4;
