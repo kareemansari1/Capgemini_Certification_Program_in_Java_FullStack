f64d8ca919e34b468251a9e2e7365374c541e3fa
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-06-04 10:36:45 +0530

    Check Connection

--- Java Works/JDBC_Connection/Statement_Delete.java
+++ Java Works/JDBC_Connection/Statement_Delete.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Delete {
+public class Statement_Delete {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		// Load and register the JDBC driver
--- Java Works/JDBC_Connection/Statement_Update.java
+++ Java Works/JDBC_Connection/Statement_Update.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Update {
+public class Statement_Update {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		// Load and register the JDBC driver
