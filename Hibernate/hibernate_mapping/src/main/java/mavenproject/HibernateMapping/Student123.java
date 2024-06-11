c9f247a53f9e27f9fbddc38693b1328b887237ca
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-06-11 12:29:27 +0530

    CRUD Operations

--- Hibernate/hibernate_mapping/src/main/java/mavenproject/HibernateMapping/Student123.java
+++ Hibernate/hibernate_mapping/src/main/java/mavenproject/HibernateMapping/Student123.java
@@ -7,52 +7,61 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Studnet123_11787") // table created automatically.
+@Table(name = "Student123_11787") // The table is created automatically with this name.
 public class Student123 {
-	@Id // primary key.
-	@GeneratedValue(strategy = GenerationType.AUTO) // id auto genrated.
+
+	@Id // Marks the field as primary key.
+	@GeneratedValue(strategy = GenerationType.AUTO) // The ID is automatically generated.
 	private int sid;
+
 	private String sname;
 	private String scity;
 
+	// Getter for sid
 	public int getSid() {
 		return sid;
 	}
 
+	// Setter for sid
 	public void setSid(int sid) {
 		this.sid = sid;
 	}
 
+	// Getter for sname
 	public String getSname() {
 		return sname;
 	}
 
+	// Setter for sname
 	public void setSname(String sname) {
 		this.sname = sname;
 	}
 
+	// Getter for scity
 	public String getScity() {
 		return scity;
 	}
 
+	// Setter for scity
 	public void setScity(String scity) {
 		this.scity = scity;
 	}
 
+	// Overriding toString method to print student details
 	@Override
 	public String toString() {
 		return "Student123 [sid=" + sid + ", sname=" + sname + ", scity=" + scity + "]";
 	}
 
+	// Parameterized constructor
 	public Student123(String sname, String scity) {
 		super();
 		this.sname = sname;
 		this.scity = scity;
 	}
 
+	// Default constructor
 	public Student123() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-
 }
