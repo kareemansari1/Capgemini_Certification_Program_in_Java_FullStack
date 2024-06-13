f489ddf180117b2708fa19ec729fb9fcc0b962ad
Kareem Ansari <112622354+kareemansari1@users.noreply.github.com>
2024-06-13 10:51:24 +0530

    One TO One Mapping

---
+++ Hibernate/hibernate_mapping.onetoone/src/main/java/onetooneunidirectional/Question.java
@@ -0,0 +1,48 @@
+package onetooneunidirectional;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity // Marks this class as an entity to be managed by JPA
+public class Question {
+	@Id // Specifies the primary key of the entity
+	@GeneratedValue(strategy = GenerationType.AUTO) // Configures the way of incrementing the specified column (qid)
+	private int qid;
+
+	private String qname;
+
+	@OneToOne // Defines a one-to-one relationship between Question and Answer
+	@JoinColumn(name = "aid") // Specifies the column that will be used for joining the two tables
+	private Answer ans;
+
+	// Getter and Setter methods for 'ans'
+	public Answer getAns() {
+		return ans;
+	}
+
+	public void setAns(Answer ans) {
+		this.ans = ans;
+	}
+
+	// Getter and Setter methods for 'qid'
+	public int getQid() {
+		return qid;
+	}
+
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+
+	// Getter and Setter methods for 'qname'
+	public String getQname() {
+		return qname;
+	}
+
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+}
